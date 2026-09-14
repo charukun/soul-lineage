f432eeff3b2b75c2e5216f996f5fc36c31769b0d
OJARU <68882498+charukun@users.noreply.github.com>
2026-09-14 15:16:47 +0900

    feat(animations): export gameplay motion quality primitives

--- packages/animations/src/index.js
+++ packages/animations/src/index.js
@@ -7,3 +7,4 @@ export * from './intersection-risk.js';
 export * from './weapon-calibration.js';
 export * from './motion-qa.js';
 export * from './motion-authoring.js';
+export * from './gameplay-motion-quality.js';
