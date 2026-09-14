1ef19788f5276cf17f1974113051e6c8f1c145ba
OJARU <68882498+charukun@users.noreply.github.com>
2026-09-14 15:57:52 +0900

    feat(animations): export motion life contracts

--- packages/animations/src/index.js
+++ packages/animations/src/index.js
@@ -8,3 +8,4 @@ export * from './weapon-calibration.js';
 export * from './motion-qa.js';
 export * from './motion-authoring.js';
 export * from './gameplay-motion-quality.js';
+export * from './motion-life.js';
