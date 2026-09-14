868a82246ee6e29df11b91564835da4013fcea5e
OJARU <68882498+charukun@users.noreply.github.com>
2026-09-14 15:59:31 +0900

    feat(rinne): route humanoid entry through life layer

--- apps/rinne/public/simulator/src/humanoid.js
+++ apps/rinne/public/simulator/src/humanoid.js
@@ -1,3 +1,4 @@
 export {catalog,motionCatalog,profiles,weaponSockets} from './humanoid-core.js';
 export {naturalArmPole,shouldNaturalizeWeaponStance} from './humanoid-natural-stance.js';
-export {HumanoidRuntime,HUMANOID_DYNAMICS_REVISION} from './humanoid-dynamics.js';
+export {HUMANOID_DYNAMICS_REVISION} from './humanoid-dynamics.js';
+export {HumanoidRuntime,HUMANOID_LIFE_REVISION} from './humanoid-life.js';
