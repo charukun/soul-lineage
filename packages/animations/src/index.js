1149340adce874b08ca9ea20ca98784df3c045c7
OJARU <68882498+charukun@users.noreply.github.com>
2026-09-14 14:17:09 +0900

    feat(animations): export motion reference benchmark

--- packages/animations/src/index.js
+++ packages/animations/src/index.js
@@ -5,4 +5,5 @@ export * from './motion-normalization.js';
 export * from './pose-sanity.js';
 export * from './intersection-risk.js';
 export * from './weapon-calibration.js';
+export * from './motion-reference-benchmark.js';
 export * from './motion-qa.js';
