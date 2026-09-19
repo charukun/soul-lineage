18fd599b39fec454474bd7c976706287c31702ae
OJARU <68882498+charukun@users.noreply.github.com>
2026-09-19 17:13:56 +0900

    Add provenance-safe multi-source VFX builders

---
+++ apps/rinne/src/rebuild/review-vfx-multisource.js
@@ -0,0 +1,10 @@
+export const reviewVfxAssetFrom=(source,{sourcePath,targetPath=sourcePath,byteLength,gitBlobSha,reviewLibrary=false})=>Object.freeze({
+  path:`review-library/${source.namespace}/${targetPath}`,sourcePath,byteLength,gitBlobSha,
+  repository:source.repository,revision:source.revision,license:source.license,reviewOnly:true,
+  ...(reviewLibrary?{reviewLibrary:true}:{})
+});
+export const reviewVfxEffectFrom=(source,id,sourcePath,author,options={})=>Object.freeze({
+  id,path:`review-library/${source.namespace}/${sourcePath}`,sourcePath,author,
+  repository:source.repository,revision:source.revision,license:source.license,
+  scale:options.scale??1,lifetime:options.lifetime??2,reviewOnly:true
+});
