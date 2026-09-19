17a6420d79e25362bae177d709b3035e4b75c08a
OJARU <68882498+charukun@users.noreply.github.com>
2026-09-19 17:36:31 +0900

    Pin additional official CC0 VFX sources

---
+++ apps/rinne/src/rebuild/review-vfx-additional-sources.js
@@ -0,0 +1,23 @@
+export const EFFECT_MATERIALS_SOURCE=Object.freeze({
+  namespace:'effect-materials',
+  repository:'effekseer/EffectMaterials',
+  revision:'8bf8edfedb51ac09af3d4cf788cb81746ea75a82',
+  license:'CC0-1.0',
+  licenseRepository:'effekseer/EffectMaterials',
+  licenseRevision:'8bf8edfedb51ac09af3d4cf788cb81746ea75a82',
+  licensePath:'README.md',
+  licenseBlob:'2c7a97258bb46a322e2c52451031e96cbd1e71fb',
+  licenseBytes:674,
+});
+export const RESOURCE_DATA_CURRENT_SOURCE=Object.freeze({
+  namespace:'resource-data-current',
+  repository:'effekseer/ResourceData',
+  revision:'54e00177497a148647373099e996d0392f28aec2',
+  license:'CC0-1.0',
+  licenseRepository:'effekseer/Effekseer',
+  licenseRevision:'82b37081a302b9f9eff0bf14dc6c845fca8c3c54',
+  licensePath:'docs/readme_sample.txt',
+  licenseBlob:'dd2fdb3f8a615965bf54be715a6c0ccbca5daae3',
+  licenseBytes:731,
+});
+export const REVIEW_VFX_ADDITIONAL_SOURCES=Object.freeze([EFFECT_MATERIALS_SOURCE,RESOURCE_DATA_CURRENT_SOURCE]);
